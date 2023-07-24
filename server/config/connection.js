5bad4038cfcc5ac715caf435c2b2c6b6b99d9397
Josh <joshua.butts4477@gmail.com>
2023-07-24 14:56:27 -0500

    fixed search and saved books

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log("connected to the database");
-});
-
-module.exports = db;
+module.exports = mongoose.connection;
