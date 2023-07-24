63abf0cef4b9cf4161a6c20847711463df60473f
Josh <joshua.butts4477@gmail.com>
2023-07-24 00:23:04 -0500

    editted components, utils, and added graphql components

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,57 +1,128 @@
-// route to get logged in user's info (needs the token)
 export const getMe = (token) => {
-  return fetch('/api/users/me', {
+  return fetch('/graphql', {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
     },
+    body: JSON.stringify({
+      query: `query {
+        me {
+          _id
+          username
+          email
+          savedBooks {
+            _id
+            authors
+            description
+            title
+            image
+            link
+          }
+        }
+      }`
+    }),
   });
 };
 
 export const createUser = (userData) => {
-  return fetch('/api/users', {
+  return fetch('/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(userData),
+    body: JSON.stringify({
+      mutation: `mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+          token
+          user {
+            _id
+            username
+          }
+        }
+      }`,
+      variables: userData,
+    }),
   });
 };
 
-export const login = (userData) => {
-  return fetch('/api/users/login', {
+export const loginUser = (userData) => {
+  return fetch('/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(userData),
+    body: JSON.stringify({
+      mutation: `mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          token
+          user {
+            _id
+            username
+          }
+        }
+      }`,
+      variables: userData,
+    }),
   });
 };
 
-// save book data for a logged in user
 export const saveBook = (bookData, token) => {
-  return fetch('/api/users', {
+  return fetch('/graphql', {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(bookData),
+    body: JSON.stringify({
+      mutation: `mutation saveBook($input: BookInput!) {
+        saveBook(input: $input) {
+          _id
+          username
+          email
+          savedBooks {
+            _id
+            authors
+            description
+            title
+            image
+            link
+          }
+        }
+      }`,
+      variables: { input: bookData },
+    }),
   });
 };
 
-// remove saved book data for a logged in user
-export const delBook = (bookId, token) => {
-  return fetch(`/api/users/books/${bookId}`, {
-    method: 'DELETE',
+export const deleteBook = (bookId, token) => {
+  return fetch(`/graphql`, {
+    method: 'POST',
     headers: {
+      'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
     },
+    body: JSON.stringify({
+      mutation: `mutation deleteBook($bookId: ID!) {
+        deleteBook(bookId: $bookId) {
+          _id
+          username
+          email
+          savedBooks {
+            _id
+            authors
+            description
+            title
+            image
+            link
+          }
+        }
+      }`,
+      variables: { bookId },
+    }),
   });
 };
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
